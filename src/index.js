34ef0ac953fb8379d507cd93fc8b698366b46c1a
hannahmosr <142176019+hannahmosr@users.noreply.github.com>
2024-01-07 11:58:31 +0100

    added search functionality using API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
+function updateWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = Math.round(response.data.temperature.current);
+  let searchCity = document.querySelector("#city");
+
+  searchCity.innerHTML = response.data.city;
+  temperatureElement.innerHTML = temperature;
+}
+
+function newCity(city) {
+  let apiKey = "7394a3ffab5cc09c86e6t0co260f5ec2";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateWeather);
+}
+
 function searchFormQuery(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let searchCity = document.querySelector("#city");
-  searchCity.innerHTML = searchInput.value;
+
+  newCity(searchInput.value);
 }
 
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", searchFormQuery);
+
+newCity("Innsbruck");
